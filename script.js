baed6bbc8dd3909a245fcc24afac45f37e9cd129
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-05-04 00:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	 const result = [];
+  let currentSubarray = [];
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentNum = arr[i];
+    if (currentSum + currentNum <= n) {
+      currentSubarray.push(currentNum);
+      currentSum += currentNum;
+    } else {
+      result.push(currentSubarray);
+      currentSubarray = [currentNum];
+      currentSum = currentNum;
+    }
+  }
+
+  // Push the remaining subarray if it's not empty
+  if (currentSubarray.length > 0) {
+    result.push(currentSubarray);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
